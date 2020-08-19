7dad914538126b5a3cf1c2d4dc612f506a528ef6
Jani Peng <jani.peng@cargosmart.com>
2020-08-19 22:45:04 +0800

    move total amount and credits method

--- src/printUtils.js
+++ src/printUtils.js
@@ -3,27 +3,10 @@ function renderPlainText (data) {
   for (let perf of data.performances) {
     result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${(totalVolumeCredits())} credits \n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${(data.totalVolumeCredits)} credits \n`;
 
   return result;
-
-  function totalAmount () {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
-
-  function totalVolumeCredits () {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
-
 
   function usd (aNumber) {
     return new Intl.NumberFormat('en-US', {
@@ -38,6 +21,8 @@ function statement (invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData);
 
   function enrichPerformance (aPerformance) {
@@ -45,6 +30,22 @@ function statement (invoice, plays) {
     result.play = playFor(aPerformance);
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
+    return result;
+  }
+
+  function totalAmount (data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
+
+  function totalVolumeCredits (data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
     return result;
   }
 
