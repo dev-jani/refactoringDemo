f6756482dea89552ddb0c42c37f8ef2766d983b1
Jani Peng <jani.peng@cargosmart.com>
2020-08-22 22:26:22 +0800

    extract function usd format

--- src/statement.js
+++ src/statement.js
@@ -20,15 +20,18 @@ function amountFor (play, perf) {
   return thisAmount;
 }
 
+function usd (thisAmount) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(thisAmount / 100);
+}
+
 const statement = (invoice, plays) => {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     const thisAmount = amountFor(play, perf);
@@ -37,10 +40,10 @@ const statement = (invoice, plays) => {
     // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
     //print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += ` ${play.name}: ${(usd(thisAmount))} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 };
