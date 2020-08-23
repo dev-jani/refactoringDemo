3d5281a964c9be79c2bb7b4b0cea8c3396ffb9d5
Jani Peng <jani.peng@cargosmart.com>
2020-08-23 10:00:41 +0800

    revert extract function createStatementData

--- src/statement.js
+++ src/statement.js
@@ -51,30 +51,14 @@ function playFor (plays, perf) {
   return plays[perf.playID];
 }
 
-function createStatementData (invoice, plays) {
-  let customer = invoice.customer;
-  for (let perf of invoice.performances) {
-    perf.play = playFor(plays, perf);
-    perf.amount = amountFor(perf.play, perf);
-  }
-  let totalAmount2 = totalAmount(invoice, plays);
-  let totalVolumeCredits = volumeCreditsFor(invoice, plays);
-  return {
-    customer,
-    totalAmount2,
-    totalVolumeCredits,
-  };
-}
-
 const statement = (invoice, plays) => {
-  let {customer, totalAmount2, totalVolumeCredits} = createStatementData(invoice, plays);
-  let result = `Statement for ${customer}\n`;
+  let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     //print line for this order
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(plays, perf).name}: ${(usd(amountFor(playFor(plays, perf), perf)))} (${perf.audience} seats)\n`;
   }
-  result += `Amount owed is ${usd(totalAmount2)}\n`;
-  result += `You earned ${totalVolumeCredits} credits \n`;
+  result += `Amount owed is ${usd(totalAmount(invoice, plays))}\n`;
+  result += `You earned ${(volumeCreditsFor(invoice, plays))} credits \n`;
   return result;
 };
 
