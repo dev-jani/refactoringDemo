2b276c552a8825956171e93d5920af77090153e5
Jani Peng <jani.peng@cargosmart.com>
2020-09-01 23:20:47 +0800

    use createRating factory function

--- src/rank.js
+++ src/rank.js
@@ -93,7 +93,7 @@ function createRating (voyage, history) {
 }
 
 function rating (voyage, history) {
-  return new Rating(voyage, history).value;
+  return createRating(voyage, history).value;
 }
 
 module.exports = {};
