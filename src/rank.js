8352fded134301525be965dbf482c9ab75882915
Jani Peng <jani.peng@cargosmart.com>
2020-09-01 23:16:57 +0800

    combine functions to class

--- src/rank.js
+++ src/rank.js
@@ -1,81 +1,92 @@
-function voyageRisk (voyage) {
-  let result = 1;
-  if (voyage.length > 4) {
-    result += 2;
+class Rating {
+  constructor (voyage, history) {
+    this._voyage = voyage;
+    this._history = history;
   }
-  if (voyage.length > 8) {
-    result += voyage.length - 8;
-  }
-  if ([
-    'china',
-    'east-indies',
-  ].includes(voyage.zone)) {
-    result += 4;
-  }
-  return Math.max(result, 0);
-}
 
-function hasChina (history) {
-  return history.some(v => 'china' === v.zone);
-}
+  get value () {
+    const vpf = this.voyageProfitFactor;
+    const vr = this.voyageRisk;
+    const chr = this.captainHistoryRisk;
+    if (vpf * 3 > (vr + chr * 2)) {
+      return 'A';
+    }
+    else {
+      return 'B';
+    }
+  }
 
-function captainHistoryRisk (voyage, history) {
-  let result = 1;
-  if (history.length < 5) {
-    result += 4;
-  }
-  result += history.filter(v => v.profit < 0).length;
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result -= 2;
-  }
-  return Math.max(result, 0);
-}
-
-function voyageProfitFactor (voyage, history) {
-  let result = 2;
-  if (voyage.zone === 'china') {
-    result += 1;
-  }
-  if (voyage.zone === 'east-indies') {
-    result += 1;
-  }
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) {
+  get voyageProfitFactor () {
+    let result = 2;
+    if (this._voyage.zone === 'china') {
       result += 1;
     }
-    if (voyage.length > 12) {
+    if (this._voyage.zone === 'east-indies') {
       result += 1;
     }
-    if (voyage.length > 18) {
-      result -= 1;
+    if (this._voyage.zone === 'china' && this.hasChina) {
+      result += 3;
+      if (this._history.length > 10) {
+        result += 1;
+      }
+      if (this._voyage.length > 12) {
+        result += 1;
+      }
+      if (this._voyage.length > 18) {
+        result -= 1;
+      }
     }
+    else {
+      if (this._history.length > 8) {
+        result += 1;
+      }
+      if (this._voyage.length > 14) {
+        result -= 1;
+      }
+    }
+    return result;
   }
-  else {
-    if (history.length > 8) {
-      result += 1;
+
+  get voyageRisk () {
+    let result = 1;
+    if (this._voyage.length > 4) {
+      result += 2;
     }
-    if (voyage.length > 14) {
-      result -= 1;
+    if (this._voyage.length > 8) {
+      result += this._voyage.length - 8;
     }
+    if ([
+      'china',
+      'east-indies',
+    ].includes(this._voyage.zone)) {
+      result += 4;
+    }
+    return Math.max(result, 0);
   }
-  return result;
+
+  get captainHistoryRisk () {
+    let result = 1;
+    if (this._history.length < 5) {
+      result += 4;
+    }
+    result += this._history.filter(v => v.profit < 0).length;
+    if (this._voyage.zone === 'china' && this.hasChina) {
+      result -= 2;
+    }
+    return Math.max(result, 0);
+  }
+
+  get hasChina () {
+    return this._history.some(v => 'china' === v.zone);
+  }
+
 }
 
 function rating (voyage, history) {
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-  if (vpf * 3 > (vr + chr * 2)) {
-    return 'A';
-  }
-  else {
-    return 'B';
-  }
+  return new Rating(voyage, history).value;
 }
 
-module.exports = {
-};
+module.exports = {};
 
 const voyage = {
   zone: 'west-indies',
@@ -85,10 +96,12 @@ const history = [
   {
     zone: 'east-indies',
     profit: 5,
-  },{
+  },
+  {
     zone: 'west-indies',
     profit: 15,
-  },{
+  },
+  {
     zone: 'china',
     profit: -2,
   },
