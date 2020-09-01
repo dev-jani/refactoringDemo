7e9aafac19865f7ca7541765b386489138ff6e07
Jani Peng <jani.peng@cargosmart.com>
2020-09-01 22:46:08 +0800

    createEmployee factory

--- src/employee.js
+++ src/employee.js
@@ -31,5 +31,9 @@ class Engineer extends Employee {
 }
 
 function createEmployee (name, type) {
+  switch (type) {
+    case 'engineer':
+      return new Engineer(name, type);
+  }
   return new Employee(name, type);
 }
